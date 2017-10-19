f1ca87aef28dd7bb8921225b93076ab8c50914ac
fisherpie <fdietz@ucmerced.edu>
2017-10-18 19:37:00 -0700

    Add files via upload

---
+++ Project 2/dataStructures1/dataStructures/linkstatetest.h
@@ -0,0 +1,14 @@
+#ifndef LINKSTATETEST_H
+#define LINKSTATETEST_H
+
+typedef nx_struct linkstatetest{
+		nx_uint8_t derpOrigin;
+		nx_uint8_t derpadorp;
+		nx_uint8_t stopcallingitderp;
+}linkstatetest;
+
+
+
+
+
+#endif /* LINKSTATETEST_H */
